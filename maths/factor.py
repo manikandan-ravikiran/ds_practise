00929fdeb46ea38d5f7220ae0f75e0656265b658
Manikandan <vishnumani.2009@rediffmail.com>
2020-12-04 18:31:36 +0530

    Added Factors Day 4

---
+++ maths/factor.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        facts=[]
+        for i in range(1,n+1):
+            if n%i==0:
+                facts.append(i)
+                if len(facts)==k:
+                    break
+        if len(facts)<k:
+            return -1
+        else:
+            return facts[-1]
